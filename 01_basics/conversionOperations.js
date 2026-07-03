bc5935a80fca13e2a375105e65ebdb4294c92a7b
unnati-web-0026 <unnatigupta0026@gmail.com>
2026-07-03 14:24:22 +0000

    update changes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,50 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abd" => NaN
+// true =>1; false =>0
+// "" => false
+// "Unnati" => true
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+//*********************Operations************************
+
+let value = 3 
+let negValue = -value 
+console.log(negValue)
+
+// console.log(2 +2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2/2)
+//console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Wrold"
+let str3 = str1 + str2 
+console.log(str3)
+
+// console.log("1" + 2) 2)
+// console.log(1 + "2")
+// console.log("1" + 2 +
+// console.log(1 + 2 + "2")
