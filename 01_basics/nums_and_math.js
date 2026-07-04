c1ae0ac8079eb0e88554510ade23601b2bba3ea7
unnati-web-0026 <unnatigupta0026@gmail.com>
2026-07-04 13:31:45 +0000

    update commit

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,32 @@
+// const score = 400
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(typeof balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 123213123.4234
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ************************Math*******************************
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(53.52))
+// console.log(Math.ceil(53.52))
+// console.log(Math.floor(53.52))
+
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
