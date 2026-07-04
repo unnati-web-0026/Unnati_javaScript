f779c4c6c898ecbb95411b3b3e1bf80bf954197f
unnati-web-0026 <unnatigupta0026@gmail.com>
2026-07-04 06:36:54 +0000

    update commit

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,6 +7,15 @@
 // console.log("2" > 1)
 // console.log("02" > 1)
 
-console.log(null > 0)
-console.log(null == 0)
-console(null >= 0 )
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0 )
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// Avoid these type of above conversion 
+
+// ====  this checks value as well as datatype 
+
+console.log("2" === 2)
