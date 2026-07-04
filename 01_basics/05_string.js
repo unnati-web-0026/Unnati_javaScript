4633514cd67231b5b84868980c007bcb71524072
unnati-web-0026 <unnatigupta0026@gmail.com>
2026-07-04 12:30:07 +0000

    update commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name = "Unnati"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+ const gameName = new String('unnatigupta')
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-11, 6)
+// console.log(anotherString)
+
+
+// const newStringOne = "  Unnati   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "http://.com/unnati%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('unnati'))
+
+console.log(gameName.split('_'))
